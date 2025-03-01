b4734453bd665e990910e498cf9b45f96cf61c31
Kacper <rybkowski84@gmail.com>
2025-03-01 15:55:12 +0100

    minor fixes

--- Lista1/Zad1.py
+++ Lista1/Zad1.py
@@ -27,13 +27,15 @@ def game(n):
     print("for ",n," throws")
     print(board)
     # Plot the result
-    plt.plot(board.keys(),[i/n for i in board.values()])
+    plt.plot(board.keys(),[i/n for i in board.values()], "--bo")
+    plt.xlabel("Pole (i)")
+    plt.ylabel("p")
     plt.title(f"Rozklad prawdopodobienstwa p(i) dla {n} rzutów")
     plt.show()
 
 def game_with_prison(n):
     '''
-    Generate a game with n-dice throws and print the result of numbers of visits of each place in a dictionary, including the prison tile, where if player stands on a field 30, then is moved on a field 10
+    Generate a game with n-dice throws and print the result of numbers of visits of each place in a dictionary, including the prison tile, where if the player stands on a field 30, then is moved on a field 10
     '''
     # Player starts at first element
     pos = 0
@@ -48,13 +50,16 @@ def game_with_prison(n):
         if(pos >= 40):
             pos = pos - 40
         a[pos] += 1
+        # Whenever Player steps on the prison, go back to field 10
         if(pos == 30):
             pos = 10
             a[pos] += 1
     print("for ",n," throws")
     print(a)
     # Plot the result
-    plt.plot(a.keys(),[i/n for i in a.values()])
+    plt.plot(a.keys(),[i/n for i in a.values()], "--bo",)
+    plt.xlabel("Pole (i)")
+    plt.ylabel("p")
     plt.title(f"Rozklad prawdopodobienstwa p(i) dla {n} rzutów z uwzglednieniem pola 'wiezienie'")
     plt.show()
 
